f29cda238ab4bf3d50b2b0bdb6494b5a11259a8a
root <root@DESKTOP-QHD4VBN.localdomain>
2021-01-26 13:09:11 +0800

    1.26改版

--- 每日上报.py
+++ 每日上报.py
@@ -15,6 +15,7 @@ DRIVERKIND = config.readline().split('"')[1]
 DRIVERPATH = config.readline().split('"')[1]
 USERNAME   = config.readline().split('"')[1]
 PASSWORD   = config.readline().split('"')[1]
+LOCATION   = config.readline().split('"')[1]
 config.close()
 driver = None
 ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_0_1 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Mobile/14A403 MicroMessenger/6.3.27 NetType/WIFI Language/zh_CN'
@@ -52,20 +53,17 @@ driver.get('https://ids.hit.edu.cn/authserver/')
 driver.find_element_by_id('mobileUsername').send_keys(USERNAME)
 driver.find_element_by_id('mobilePassword').send_keys(PASSWORD)
 driver.find_element_by_id('load').click()
-driver.get('https://xg.hit.edu.cn/zhxy-xgzs/xg_mobile/shsj/loginChange')
-driver.execute_script('tongyishenfen();')
-driver.find_element_by_id('mrsb').click()
+driver.get('https://xg.hit.edu.cn/zhxy-xgzs/xg_mobile/xs/yqxx')
 driver.find_element_by_class_name('right_btn').click()
 sleep(1)
 alert = EC.alert_is_present()(driver)
 if alert:
 	alert.accept()
 	driver.find_element_by_id('center').find_elements_by_tag_name('div')[5].click()
-temp1 = driver.find_element_by_id('tw')
-temp2 = driver.find_element_by_id('tw1')
-driver.execute_script('arguments[0].setAttribute("data-action","36")', temp1)
-driver.execute_script('arguments[0].setAttribute("data-action","5")', temp2)
-driver.find_element_by_id('txfscheckbox').click()
-driver.execute_script('save();')
+loc = driver.find_element_by_id('gnxxdz')
+driver.execute_script('arguments[0].value="'+LOCATION+'"', loc)
+driver.find_element_by_id('checkbox').click()
+driver.execute_script('save()')
+driver.execute_script('document.getElementsByClassName("weui-dialog__btn primary")[0].click()')
 driver.quit()
 print('上报完成')
