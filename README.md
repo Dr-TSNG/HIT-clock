4ff1139abe302efc35af6aa40b97e045112d7258
Dr-TSNG <transhumanist@foxmail.com>
2021-05-15 18:05:13 +0800

    Test GitHub actions

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # 某HIT每日上报自动脚本  
-## 环境要求  
-python3、edge/chrome
-## 脚本使用方法  
-1.下载你电脑浏览器对应的driver，具体可以在百度上搜索edge/chrome driver  
-2.在配置文件里填入浏览器类型、driver地址、学号、密码（统一身份认证）  
-3.运行每日上报.bat，可以把它放入自启文件夹中自动打卡
+## 环境要求
+无
+## 使用方法
+Fork本项目，在Secrets中配置好ID（学号）、PASSWORD（密码）、LOCATION（位置），搞定，白嫖GitHub服务器帮你打卡
--- 每日上报.py
+++ 每日上报.py
@@ -1,53 +1,19 @@
 import os
 from time import sleep
-try:
-	from selenium import webdriver
-	from selenium.webdriver.support import expected_conditions as EC
-except:
-	print('检测到有未安装的支持库，正在安装')
-	os.system('pip3 install selenium')
-	print('安装完成，请重新运行脚本')
-	exit(0)
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
 
 print('初始化浏览器')
-config = open('配置.ini', encoding="UTF-8")
-DRIVERKIND = config.readline().split('"')[1]
-DRIVERPATH = config.readline().split('"')[1]
-USERNAME   = config.readline().split('"')[1]
-PASSWORD   = config.readline().split('"')[1]
-LOCATION   = config.readline().split('"')[1]
-config.close()
+USERNAME   = os.environ['ID']
+PASSWORD   = os.environ['PASSWORD']
+LOCATION   = os.environ['LOCATION']
 driver = None
 ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_0_1 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Mobile/14A403 MicroMessenger/6.3.27 NetType/WIFI Language/zh_CN'
-if DRIVERKIND == 'edge':
-	try:
-		from msedge.selenium_tools import Edge, EdgeOptions
-	except:
-		print('检测到有未安装的支持库，正在安装')
-		os.system('pip3 install msedge.selenium_tools')
-		sleep(1)
-		from msedge.selenium_tools import Edge, EdgeOptions
-	option = EdgeOptions()
-	option.use_chromium = True
-	option.headless = True
-	option.add_argument('user-agent='+ua)
-	try:
-		driver = Edge(executable_path = DRIVERPATH, options = option)
-	except:
-		print('driver无效，请检查设置')
-		exit(1)
-elif DRIVERKIND == 'chrome':
-	option = webdriver.ChromeOptions()
-	option.headless = True
-	option.add_argument('user-agent='+ua)
-	try:
-		driver = webdriver.Chrome(executable_path= DRIVERPATH, options = option)
-	except:
-		print('driver无效，请检查设置')
-		exit(1)
-else:
-	print('不支持的浏览器类型')
-	exit(2)
+option = webdriver.ChromeOptions()
+option.headless = True
+option.add_argument('user-agent='+ua)
+driver = webdriver.Chrome(executable_path= '/usr/bin/chromedriver', options = option)
+
 print('正在上报')
 driver.get('https://ids.hit.edu.cn/authserver/')
 driver.find_element_by_id('mobileUsername').send_keys(USERNAME)
@@ -66,4 +32,5 @@ driver.find_element_by_id('checkbox').click()
 driver.execute_script('save()')
 driver.execute_script('document.getElementsByClassName("weui-dialog__btn primary")[0].click()')
 driver.quit()
+
 print('上报完成')
