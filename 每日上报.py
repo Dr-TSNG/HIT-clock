b6e89aca8f46ecd6d76c3da8f663b626992db01b
Dr-TSNG <transhumanist@foxmail.com>
2021-06-26 10:24:01 +0800

    Try to fix

--- 每日上报.py
+++ 每日上报.py
@@ -15,9 +15,9 @@ driver = webdriver.Chrome(executable_path= '/usr/bin/chromedriver', options = op
 
 print('正在上报')
 driver.get('http://ivpn.hit.edu.cn')
-driver.find_element_by_id('username').send_keys(USERNAME)
-driver.find_element_by_id('password').send_keys(PASSWORD)
-driver.find_element_by_id('casLoginForm').find_element_by_css_selector('[type="submit"]').click()
+driver.find_element_by_id('mobileUsername').send_keys(USERNAME)
+driver.find_element_by_id('mobilePassword').send_keys(PASSWORD)
+driver.find_element_by_id('load').click()
 driver.get('http://xg-hit-edu-cn-s.ivpn.hit.edu.cn:1080/zhxy-xgzs/xg_mobile/xs/yqxx')
 driver.find_element_by_class_name('right_btn').click()
 sleep(1)
