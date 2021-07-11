781ca6c752729b26cb216bd1ea6f88e96ce46260
Nullptr <tsng.nullptr@outlook.com>
2021-07-11 22:13:45 +0800

    加入失败重试

--- 每日上报.py
+++ 每日上报.py
@@ -18,24 +18,34 @@ driver.get('http://ivpn.hit.edu.cn')
 driver.find_element_by_id('mobileUsername').send_keys(USERNAME)
 driver.find_element_by_id('mobilePassword').send_keys(PASSWORD)
 driver.find_element_by_id('load').click()
-driver.get('http://xg-hit-edu-cn-s.ivpn.hit.edu.cn:1080/zhxy-xgzs/xg_mobile/xs/yqxx')
-driver.find_element_by_class_name('right_btn').click()
-sleep(1)
-alert = EC.alert_is_present()(driver)
 
-if alert: # 重复上报
-	alert.accept()
-	driver.find_element_by_id('center').find_elements_by_tag_name('div')[5].click()
+success = False
+for i in range (0, 5):
+	try:
+		driver.get('http://xg-hit-edu-cn-s.ivpn.hit.edu.cn:1080/zhxy-xgzs/xg_mobile/xs/yqxx')
+		driver.find_element_by_class_name('right_btn').click()
+		sleep(1)
+		alert = EC.alert_is_present()(driver)
 
-alert = EC.alert_is_present()(driver)
-if alert: # 获取位置
-	alert.dismiss()
+		if alert: # 重复上报
+			alert.accept()
+			driver.find_element_by_id('center').find_elements_by_tag_name('div')[5].click()
 
-loc = driver.find_element_by_id('gnxxdz')
-driver.execute_script('arguments[0].value="'+LOCATION+'"', loc)
-driver.find_element_by_id('checkbox').click()
-driver.execute_script('save()')
-driver.execute_script('document.getElementsByClassName("weui-dialog__btn primary")[0].click()')
-driver.quit()
+		alert = EC.alert_is_present()(driver)
+		if alert: # 获取位置
+			alert.dismiss()
 
-print('上报完成')
+		loc = driver.find_element_by_id('gnxxdz')
+		driver.execute_script('arguments[0].value="'+LOCATION+'"', loc)
+		driver.find_element_by_id('checkbox').click()
+		driver.execute_script('save()')
+		driver.execute_script('document.getElementsByClassName("weui-dialog__btn primary")[0].click()')
+		driver.quit()
+		success = True
+		break
+	except:
+		print('失败' + str(i+1) + '次，正在重试...')
+if success:
+	print('上报完成')
+else:
+	raise Exception('上报多次失败，可能学工系统已更新')
