f5ced57d1e8e4841c9d4c0a817ba9a5cce8b26e8
icecity96 <liuhiter@gmail.com>
2021-08-19 10:28:01 +0800

    Update 每日上报.py

--- 每日上报.py
+++ 每日上报.py
@@ -14,28 +14,24 @@ option.add_argument('user-agent='+ua)
 driver = webdriver.Chrome(executable_path= '/usr/bin/chromedriver', options = option)
 
 print('正在上报')
-driver.get('http://ivpn.hit.edu.cn')
+driver.get('https://ids.hit.edu.cn/authserver/')
 driver.find_element_by_id('mobileUsername').send_keys(USERNAME)
 driver.find_element_by_id('mobilePassword').send_keys(PASSWORD)
 driver.find_element_by_id('load').click()
-driver.get('http://xg-hit-edu-cn-s.ivpn.hit.edu.cn:1080/zhxy-xgzs/xg_mobile/xs/yqxx')
-driver.find_element_by_class_name('right_btn').click()
-sleep(1)
-alert = EC.alert_is_present()(driver)
 
-if alert: # 重复上报
-	alert.accept()
-	driver.find_element_by_id('center').find_elements_by_tag_name('div')[5].click()
-
-alert = EC.alert_is_present()(driver)
-if alert: # 获取位置
-	alert.dismiss()
-
-loc = driver.find_element_by_id('gnxxdz')
-driver.execute_script('arguments[0].value="'+LOCATION+'"', loc)
-driver.find_element_by_id('checkbox').click()
-driver.execute_script('save()')
-driver.execute_script('document.getElementsByClassName("weui-dialog__btn primary")[0].click()')
+success = False
+for i in range (0, 5):
+	try:
+		driver.get('https://xg.hit.edu.cn/zhxy-xgzs/xg_mobile/xsMrsbNew')
+		driver.execute_script(f'kzl10 = "{LOCATION}"')
+		driver.execute_script('document.getElementById("txfscheckbox").click()')
+		driver.find_element_by_class_name('submit').click()
+		success = True
+		break
+	except:
+		print('失败' + str(i+1) + '次，正在重试...')
 driver.quit()
-
-print('上报完成')
+if success:
+	print('上报完成')
+else:
+	raise Exception('上报多次失败，可能学工系统已更新')
