2cefae91e6384b61e7564cacbbecc86e46b3a1c0
icecity96 <liuhiter@gmail.com>
2021-09-15 10:33:27 +0800

    bug fixed: 疫苗接种情况 (#13)
    * bug fixed: 修复可能出现的疫苗接种情况被设置成默认未接种

--- 每日上报.py
+++ 每日上报.py
@@ -24,6 +24,8 @@ for i in range (0, 5):
 	try:
 		driver.get('https://xg.hit.edu.cn/zhxy-xgzs/xg_mobile/xsMrsbNew')
 		driver.execute_script(f'kzl10 = "{LOCATION}"')
+		driver.execute_script('document.getElementById("kzl18-0").click()')
+		driver.execute_script('document.getElementById("kzl32-2").click()')
 		driver.execute_script('document.getElementById("txfscheckbox").click()')
 		driver.find_element_by_class_name('submit').click()
 		success = True
