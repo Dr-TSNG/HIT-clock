e6b73ab08f6587167e041a9f0f4408d7da4f0476
Ryan Loil <50242494+RyanLoil@users.noreply.github.com>
2022-04-03 12:58:54 +0800

    Add Captcha OCR

--- 每日上报.py
+++ 每日上报.py
@@ -42,8 +42,8 @@ def yzm():
 		imgelement = driver.find_element_by_xpath('//*[@id="imgObjjgRegist"]')  # 定位验证码
 		location = imgelement.location  # 获取验证码x,y轴坐标
 		size = imgelement.size  # 获取验证码的长宽
-		rangle = (int(location['x']), int(location['y']), int(location['x'] + size['width']),
-			  int(location['y'] + size['height']))  # 写成我们需要截取的位置坐标
+		rangle = (int(location['x']), int(location['y'] / 10), int(location['x'] + size['width']),
+			  int(location['y'] / 10 + size['height']))  # 写成我们需要截取的位置坐标
 		i = Image.open("./printscreen.png")
 		frame4 = i.crop(rangle)  # 使用Image的crop函数，从截图中再次截取我们需要的区域
 		frame4.save('./save.png') # 保存我们接下来的验证码图片 进行打码
